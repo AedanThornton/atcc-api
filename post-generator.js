2faea94a9ca51f460bf3a4a8438fed3f67690eb3
AedanThornton <ajt1111@outlook.com>
2026-05-13 09:53:22 -0500

    New: Unrelated discord post generator

---
+++ post-generator.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+
+function cycleToNumber(cycleStr) {
+  const convert = {
+    "Cycle I": 1,
+    "Cycle II": 2,
+    "Cycle III": 3,
+    "Cycle IV": 4,
+    "Cycle V": 5,
+    "Mnestis Theatre": "Unknown - Theatre",
+  }
+
+  return convert[cycleStr] || "N/A"
+}
+
+function createPost({ day }) {
+  fs.readFile('./data/JSON/gearData.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error reading file:', err);
+      return;
+    }
+    const gearCards = JSON.parse(data);
+    const todaysCard = gearCards[day - 1]
+
+    const gearType = todaysCard.slot.includes("Hand") ? "Weapon" : todaysCard.slot
+    const defStats = todaysCard.defensiveStatistics.map((stat) => `* ${stat.type} ${stat.amount}`).join("\n")
+    const abilities = todaysCard.abilities.map(
+      (sentence) => {
+        let output = "* "
+        output = output + (sentence.costs ? sentence.costs.join(" ") + " " : "")
+        output = output + sentence.abilityText.map((ability) => ability.value).join("")
+        return output
+      }).join("\n")
+
+    console.log(`
+Day ${day} 
+
+>>> __**${todaysCard.name}**__
+**Power Level**: ${cycleToNumber(todaysCard.cycle)}
+**Power Tier**: -
+
+**Gear type**: ${gearType}
+**Slots used**: ${todaysCard.slot}
+__Offensive Statistics__:
+${Object.keys(todaysCard.offensiveStatistics).length > 0 ?
+        `* **Attack Dice**: ${todaysCard.offensiveStatistics.attackDice}
+* **To-Hit bonus**: ${todaysCard.offensiveStatistics.precision}
+* **Power dice**: ${JSON.stringify(todaysCard.offensiveStatistics.power)}` :
+        "* None"
+      }
+
+__Defensive Statistics__:
+${defStats || "* None"}
+
+__Abilities__:
+${abilities || "* None"}
+
+**Flavor Text**: *${todaysCard.flavor}*
+**Traits**: ${todaysCard.traits.join(", ")}
+
+`)
+  });
+}
+
+createPost({ day: 28 })
